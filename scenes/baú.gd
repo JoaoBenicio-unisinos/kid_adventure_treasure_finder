4e08b73e265f4733ea61a072496f8472740e5590
NickMB1 <NBellver@edu.unisinos.br>
2025-07-03 21:55:13 -0300

    Wasd e  Bau

---
+++ scenes/baú.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var abrir = $abrir as AnimatedSprite2D
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(_body:CharacterBody2D):
+		abrir.play("abrir")
+		$Timer.start(2)
+
+func _on_timer_timeout() -> void:
+		queue_free()
---
+++ scripts/global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var coins := 0
+var sapos := 0
+var baus := 0
